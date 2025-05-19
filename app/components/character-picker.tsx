3dab7a8ff5f7635316f99da7a623e09f0f3cec4a
Shannor Trotty <shannor.a.trotty@gmail.com>
2025-05-18 22:46:23 -0400

    Slight update to the UI when picking a character.
    Signed-off-by: Shannor Trotty <shannor.a.trotty@gmail.com>

--- app/components/character-picker.tsx
+++ app/components/character-picker.tsx
@@ -11,18 +11,23 @@ export function CharacterPicker({
   characters,
   currentCharacterId,
 }: Props) {
+  const character = characters.find((c) => c.id === currentCharacterId);
   return (
-    <div className="scroll-m-20 flex flex-col gap-4">
-      {!currentCharacterId && (
-        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-          Pick your guardian to get started
-        </h4>
+    <div className="flex scroll-m-20 flex-col gap-4">
+      {!currentCharacterId ? (
+        <h3 className="scroll-m-20 self-center text-2xl font-semibold tracking-tight">
+          Pick a Guardian
+        </h3>
+      ) : (
+        <h3 className="scroll-m-20 self-center text-2xl font-semibold tracking-tight">
+          {character?.class} Selected
+        </h3>
       )}
-      <div className="flex flex-col md:flex-row gap-4 flex-wrap justify-center">
+      <div className="flex flex-col justify-center gap-6">
         {characters.map((it) => (
           <div
             className={cn(
-              'w-[300px]',
+              'w-[300px] md:w-[400px]',
               it.id !== currentCharacterId &&
                 'cursor-pointer drop-shadow-md hover:drop-shadow-xl dark:hover:border-2 dark:hover:border-yellow-300',
               it.id === currentCharacterId &&
@@ -39,15 +44,19 @@ export function CharacterPicker({
                 backgroundSize: 'cover',
                 height: 75,
               }}
-              className="flex flex-row pl-20 p-2 align-middle justify-between"
+              className="flex flex-row justify-between p-2 pl-20 align-middle"
             >
               <div>
-                <h2 className="text-white text-lg font-semibold">{it.class}</h2>
+                <h2 className="text-lg font-semibold tracking-wider text-white">
+                  {it.class}
+                </h2>
                 <div>
-                  <div className="text-white">{it.currentTitle}</div>
+                  <div className="text-white">
+                    {it.currentTitle} - {it.race}
+                  </div>
                 </div>
               </div>
-              <div className="text-yellow-300 font-semibold text-xl drop-shadow-[0_0_2px_black]">
+              <div className="text-xl font-semibold text-yellow-300 drop-shadow-[0_0_2px_black]">
                 {it.light}
               </div>
             </div>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -30,7 +30,7 @@ export default function Home() {
       <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
         Welcome, {profile?.displayName}!
       </h2>
-      <div className="flex flex-col justify-center align-middle w-full">
+      <div className="grid place-items-center">
         <CharacterPicker
           onSubmit={(characterId) => {
             const data = new FormData();
@@ -44,26 +44,6 @@ export default function Home() {
           characters={profile.characters}
           currentCharacterId={characterId}
         />
-      </div>
-      <div className="grid grid-cols-1 gap-4">
-        <Card className="w-1/3">
-          <CardHeader>
-            <CardTitle>Getting Started</CardTitle>
-          </CardHeader>
-          <CardContent>
-            Check out the two links below to get started! You'll want to take
-            some snapshots of a guardian first to see them in the respective
-            activity.
-          </CardContent>
-          <CardFooter className="flex justify-between">
-            <Button asChild>
-              <Link to="/activities">Go to Activities</Link>
-            </Button>
-            <Button asChild>
-              <Link to="/snapshots">Go to Snapshots</Link>
-            </Button>
-          </CardFooter>
-        </Card>
       </div>
     </div>
   );
